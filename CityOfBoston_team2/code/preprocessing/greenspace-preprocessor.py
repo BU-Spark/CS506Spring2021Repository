1258ae966a899fe0c88ac1ce77106d1df1f5fe6b
Sandra Zhen <szhen8930@gmail.com>
2021-03-06 20:00:12 -0500

    filtering out relevant cols

--- CityOfBoston_team2/code/preprocessing/greenspace-preprocessor.py
+++ CityOfBoston_team2/code/preprocessing/greenspace-preprocessor.py
@@ -2,7 +2,7 @@ import pandas as pd
 from pandas import DataFrame
 
 def import_data(filename):
-    data = pd.read_csv(filename, sep=',', usecols = ['DISTRICT','ACRES','ADDRESS'])
+    data = pd.read_csv(filename, sep=',', usecols = ['DISTRICT','ZonAgg','TypeLong','ACRES','ADDRESS'])
     return data
 
 print(import_data("../../datasets/open_space.csv"))
