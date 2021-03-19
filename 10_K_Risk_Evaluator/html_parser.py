c4ae0de7d3dda3f215e2e0e2fb25b9cf230532a8
Nicholas <njmosca@bu.edu>
2021-03-19 12:37:39 -0400

    writing csv files

--- 10_K_Risk_Evaluator/html_parser.py
+++ 10_K_Risk_Evaluator/html_parser.py
@@ -83,23 +83,27 @@ def clean_strings(input_list):
 
     # Tokenize strings (i.e., break sentences down into a list of tokens)
     temp = ' '.join(input_list)  # Join elements in list into single string
-    temp = regexp_tokenize(temp, "[\w']+")  # Use a regex to split words
+    #temp = regexp_tokenize(temp, "[\w']+")  # Use a regex to split words
 
     # Lemmatization (i.e., grouping together any inflected forms of a word)
-    lemmatizer = WordNetLemmatizer()
-    temp = [lemmatizer.lemmatize(word) for word in temp]
+    # lemmatizer = WordNetLemmatizer()
+    # temp = [lemmatizer.lemmatize(word) for word in temp]
 
     # Uncomment to see how the lemmatization class works
     # for w in temp:
     #     print(w, " : ", lemmatizer.lemmatize(w))
 
     # Removing information-poor stopwords (e.g., 'in', 'the' 'to', etc.)
-    stop_words = stopwords.words('english')
-    temp = [w for w in temp if w not in stop_words]
+    # stop_words = stopwords.words('english')
+    # temp = [w for w in temp if w not in stop_words]
 
-    # Remove digit-value and single-character strings from text
-    temp = [x for x in temp if not (x.isdigit())]
-    cleaned_list = [x for x in temp if len(x) > 1]
+    # # Remove digit-value and single-character strings from text
+    # temp = [x for x in temp if not (x.isdigit())]
+    # cleaned_list = [x for x in temp if len(x) > 1]
+
+    return temp
+
+
 
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -113,9 +117,8 @@ def main_path(path):
     end_terms = ['ITEM', '1B.', 'UNRESOLVED', 'STAFF', 'COMMENTS']
 
     risk_text = grab_section_text(tree, start_terms, end_terms)
-    import pdb; pdb.set_trace()
     risk_text = clean_strings(risk_text)
-    
+    #import pdb; pdb.set_trace()
     # print(risk_text)
     # print(len(risk_text))
 
@@ -125,8 +128,12 @@ def main_path(path):
     #cleaned_list.append(all_text)
 
 
+<<<<<<< HEAD
     return all_text
 >>>>>>>  working on structure data branch
+=======
+    return risk_text
+>>>>>>> writing csv files
 
 
 def main():
