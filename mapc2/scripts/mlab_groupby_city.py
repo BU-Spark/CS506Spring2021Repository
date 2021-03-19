f3f4842c9faa5b221f5d03086f6e429841e2d5cf
Jenny Li <jennjenn@bu.edu>
2021-03-19 03:18:41 -0400

    py file to grab data by city

--- mapc2/scripts/mlab_groupby_city.py
+++ mapc2/scripts/mlab_groupby_city.py
@@ -1,35 +1,46 @@
 import pandas as pd
 import numpy as np 
 
-mapc = pd.read_csv('../data/tabular_datakeys_muni351.csv')
+mapc = pd.read_csv('../data/mapc_municipalities.csv')
 
 filepath = '../data/mlab_2020_with_provider_name.csv' # 'mapc2\data\mlab_2020_with_provider_name.csv'
 mLab = pd.read_csv(filepath)
 
 writepath = '../data/mlab_2020_by_city/'
 
-def data_to_csv(data, city):
-    print('Converting ' + city + ' data to csv')
-    data.to_csv(writepath + city + '_mlab_2020.csv', index=False)
+def data_to_csv(data, value):
+    print('Converting ' + value + ' data to csv')
+    data.to_csv(writepath + value + '_mlab_2020.csv', index=False)
+    # data.to_csv('../data/mapc_municipalities.csv', index=False)
 
-def group_data_by_field(df, fieldname, value):
+def group_data_by_value(df, fieldname, value):
     data = df[df[fieldname] == value]
-    # print(city_data.columns)
     data_to_csv(data, value)
 
 def _get_all_fieldValues(df, field):
     unique = df[field].unique()
     return unique
 
-def groupby_field(df, field):
+def group_data_by_field(df, field):
     unique = _get_all_fieldValues(df, field)
-    # print(cities)
     for value in unique:
         if type(value) == str:
-            group_data_by_field(df, city)
+            group_data_by_value(df, field, value)
         else:
-            print(city)
+            print(value)
 
-# mLab.set_index('Index')
-group_data(mLab)
-# print(mLab.columns)
+def match_data_by_field(df, field, match_df, match_field):
+    match = _get_all_fieldValues(match_df, match_field)
+    for value in match:
+        if type(value) == str:
+            group_data_by_value(df, field, value)
+        else:
+            print(value)
+
+# group_data_by_field(mLab, 'City')
+# group_data_by_value(mapc, 'region', 'MAPC')
+match_data_by_field(mLab, 'City', mapc, 'municipal')
+
+
+# print(mapc['muni_upper'].value_counts())
+# print(mapc['municipal'].value_counts())
