8f5be8b82f24cc3948cb0bf53be6ff0f4fc1baf5
George Padavick <george.padavick@gmail.com>
2021-03-24 21:14:39 -0400

    Added changes for deliverable 2

--- gilgo_padavick_final_project/code/data_aquisition/twitter_data.py
+++ gilgo_padavick_final_project/code/data_aquisition/twitter_data.py
@@ -3,16 +3,19 @@ import json
 import tweepy
 import datetime as dt
 import pandas as pd
+import os
 
-with open('keys.json') as keys:
+cur_path = os.path.dirname(__file__)
+file_path = cur_path + '\\keys.json'
+
+with open(file_path) as keys:
     config = json.load(keys)
 auth = tweepy.OAuthHandler(config['twitter']['API_key'], config['twitter']['API_secret_key'])
-# auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
-def get_stock_tweets_list(stock, start_date, end_date, max_tweets=10000):
-    searched_tweets = [status for status in tweepy.Cursor(api.search, q="${}".format(stock), since=start_date, until=end_date).items(max_tweets)]
+def get_stock_tweets_list(stock, start_date, end_date=None):
+    searched_tweets = [status for status in tweepy.Cursor(api.search, q="${}".format(stock), since=start_date, until=end_date).items()]
     return searched_tweets
 
 def generate_df(stocks, start_date, end_date):
@@ -23,6 +26,7 @@ def generate_df(stocks, start_date, end_date):
         stock_data = pd.io.json.json_normalize(json_data)
         stock_data['stock'] = stock
         data = data.append(stock_data)
+    data['created_at'] = pd.to_datetime(data['created_at'])
     data = data.set_index('created_at')
     return data
 
