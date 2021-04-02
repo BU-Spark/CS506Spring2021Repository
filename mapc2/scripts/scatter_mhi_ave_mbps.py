00c9feb5f47a874cced66b452e6e5706e325c292
Nathan Lauer <lauern91@gmail.com>
2021-04-02 13:46:19 -0400

    Ookla images and some updates to scatter script

--- mapc2/scripts/scatter_mhi_ave_mbps.py
+++ mapc2/scripts/scatter_mhi_ave_mbps.py
@@ -20,6 +20,8 @@ parser = argparse.ArgumentParser(description='Scatter of MBPS against MHI')
 parser.add_argument('--mhi-file', dest="mhi_file", help="Census file")
 parser.add_argument('--mlab-file', dest="mlab_file", help="MLAB data")
 parser.add_argument('--mapc-file', dest="mapc_file", help="MAPC municipalities file")
+parser.add_argument('--speed-col', dest="speed_col", help='name of column that has speed value')
+parser.add_argument('--output', dest="out_file", help="Outputfile")
 args = parser.parse_args()
 
 # Read input files
@@ -27,15 +29,21 @@ df_mhi = pd.read_csv(args.mhi_file)
 df_mlab = pd.read_csv(args.mlab_file)
 mapc_cities = pd.read_csv(args.mapc_file)['municipal'].to_list()
 
+# Possibly rename municipal column in input file
+if 'municipal' in df_mlab.columns:
+  df_mlab = df_mlab.rename(columns={"municipal": "City"})
+
 # First, compute average MBPS per city in mlab data
-mlab_avg_mbps = df_mlab.groupby(['City']).mean().drop(columns=['MinRTT', 'Latitude'  ,'Longitude' ,'ProviderNumber'])
+# mlab_avg_mbps = df_mlab.groupby(['City']).mean().drop(columns=['MinRTT', 'Latitude'  ,'Longitude' ,'ProviderNumber'])
+mlab_avg_mbps = df_mlab.groupby(['City']).mean() #.drop(columns=['MinRTT', 'Latitude'  ,'Longitude' ,'ProviderNumber'])
+mlab_avg_mbps = mlab_avg_mbps[[args.speed_col]]
 
 # Rename 'municipal' in MHI file to City
 df_mhi['City'] = df_mhi['municipal']
 
 # Join mlab_avg_mbps and df_mhi
 joined = pd.merge(df_mhi, mlab_avg_mbps, on="City")
-joined = joined[['mhi', 'City', 'MeanThroughputMbps']]
+joined = joined[['mhi', 'City', args.speed_col]]
 # Note: the join "auto-removed" any cities that did not appear in both data sets.
 # It would be interesting to have a list of these.
 
@@ -48,7 +56,7 @@ colors = np.array([in_color if city == 1 else out_color for city in joined['InMa
 # Plot City against mhi
 fig, ax = plt.subplots(figsize=(15,10))
 x = joined['mhi']
-y = joined['MeanThroughputMbps']
+y = joined[args.speed_col]
 scatter = ax.scatter(x, y, alpha=0.9, c=colors)
 
 # Label each data point with the associated city
@@ -60,7 +68,7 @@ mpld3.plugins.connect(fig, tooltip)
 ax.set_xlabel("Median Household Income, 2014-2018, Dollars", size=20)
 ax.set_ylabel("Mean Throughput Mbps, MLAB 2020 data", size=20)
 ax.set_title("MLAB: Average Broadband Speed against Median Household Income", size=30)
-xvalues = np.arange(40000,240000, 20000)
+xvalues = np.arange(40000, 240000, 20000)
 yvalues = np.arange(0, 400, 50)
 ax.set_xticklabels(xvalues, fontsize=16)
 ax.set_yticklabels(yvalues, fontsize=16)
@@ -80,13 +88,13 @@ in_mapc_data = joined[joined['InMapc'] == 1]
 out_mapc_data = joined[joined['InMapc'] != 1]
 # In MAPC trendline
 x_in = in_mapc_data['mhi']
-y_in = in_mapc_data['MeanThroughputMbps']
+y_in = in_mapc_data[args.speed_col]
 z_in = np.polyfit(x_in, y_in, 1)
 p_in = np.poly1d(z_in)
 ax.plot(x_in,p_in(x_in),"--", c=in_color, label='MAPC Tracked Municipalities')
 # Not in MAPC trendline
 x_out = out_mapc_data['mhi']
-y_out = out_mapc_data['MeanThroughputMbps']
+y_out = out_mapc_data[args.speed_col]
 z_out = np.polyfit(x_out, y_out, 1)
 p_out = np.poly1d(z_out)
 ax.plot(x_out,p_out(x_out),"--", c=out_color, label='Non-Tracked Municipalities')
@@ -95,8 +103,12 @@ ax.plot(x_out,p_out(x_out),"--", c=out_color, label='Non-Tracked Municipalities'
 ax.legend()
 # mpld3.show()
 
-mpld3.save_html(fig, "output/mlab_mhi_vs_mbps_2020.html")
+mpld3.save_html(fig, args.out_file)
 
 # Some ideas:
 # - Produce second plot, with data points that are above 150 MBPS removed
-# - Trend lines for each category of municipalities
+# - Modify xvalues and yvalues to go from min to max, not hard coded.
+# - Write scripts that automate the input values into this python script
+# - Pass args for title, xlabel, and ylabel.
+# - Modify tooltip to include MHI and speed value
+# - Produce modified ookla data that has speeds measured in mbps, not just kbps.
