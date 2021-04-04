4d9816b7a22d3a735df6d118de3406eae5a849d3
serra98 <serra.jung@gmail.com>
2021-04-04 15:44:25 -0700

    updated train_action.p to use RandomForest and MultinomialNB
    It also includes the output data on first 10000 samples.

--- Civera/Code/train_action.py
+++ Civera/Code/train_action.py
@@ -3,7 +3,9 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import re
+from csv import writer
 import copy
+import os 
 import matplotlib.pyplot as plt
 from nltk.stem import WordNetLemmatizer 
 from nltk.corpus import stopwords
@@ -12,11 +14,12 @@ from nltk.tokenize import word_tokenize, sent_tokenize
 from sklearn.preprocessing import StandardScaler, MinMaxScaler, PolynomialFeatures
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.model_selection import train_test_split
-
+from sklearn.ensemble import VotingClassifier
 from sklearn.metrics import mean_squared_error, confusion_matrix
 from sklearn.pipeline import Pipeline
 from sklearn.svm import LinearSVC
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import MultinomialNB
 
 import mysql.connector
 
@@ -29,44 +32,51 @@ else:
 
 mycursor = mydb.cursor()
 
-#load sql to dataframe 
-case_index_not_null = pd.read_sql("SELECT * FROM wp_courtdocs.cdocs_case_action_index as c_a_index WHERE c_a_index.action != ' ' and c_a_index.actor != ' ' and rand() <= .2;", con = mydb)
 
+# Load sql to dataframe 
+# Get Training Set (Action != NULL and Actor != NULL)
+# Getting 10000 values first 
+case_index_not_null = pd.read_sql("SELECT * FROM wp_courtdocs.cdocs_case_action_index as c_a_index WHERE c_a_index.action != ' ' and c_a_index.actor != ' ' LIMIT 10000;", con = mydb)
 columns = ['actor','action','description']
 trainSet = case_index_not_null[columns]
 print(trainSet.head())
 
-# cdocs_case_action_index / actor = null
-action_null = pd.read_sql("SELECT * FROM wp_courtdocs.cdocs_case_action_index as c_a_index WHERE c_a_index.action = ' ' and c_a_index.actor != ' ' and rand() <= .2;", con = mydb)
+# Get Test Set (Action = NULL)
+# Getting 10000 values first 
+action_null = pd.read_sql("SELECT * FROM wp_courtdocs.cdocs_case_action_index as c_a_index WHERE c_a_index.action = ' ' LIMIT 10000;", con = mydb)
 testSet = action_null[columns]
 print(testSet.head())
 
+# Get Distinct Values of Actions Field with Index Number 
+path1 = "C:\\Users\\Serra\\Desktop\\CS506Spring2021Repository\\Civera\\Data\\distinct-case-actions.txt"
+distinct_actions = pd.read_csv(path1)
+print(distinct_actions.head())
 
-# X = trainingSet[['action','description']]
-# y = trainingSet['actor']
-
-# X_train, X_test, y_train, y_test = train_test_split(X,y,test_size = 0.2, random_state = 42)
-
-#distinct values
-df = pd.read_csv('./Civera/Data/distinct-case-actions.csv', index_col=int)
-print(df.head())
-
+#merge training set with distinct-case-actions.txt to get the index value for distinct actions 
+trainSet = trainSet.merge(distinct_actions, on='action')
+print(trainSet.head())
 
 r = re.compile(r'[^\w\s]+')
 trainSet['description'] = [r.sub('', x) for x in trainSet['description'].tolist()]
 trainSet['description'] = trainSet['description'].str.lower().str.split()
+print(trainSet.head())
+
 testSet['description'] = [r.sub('', x) for x in testSet['description'].tolist()]
 testSet['description'] = testSet['description'].str.lower().str.split()
+print(testSet.head())
 
 stopwords = stopwords.words('english')
+#remove stopwords in trainSet 
 trainSet['description'] = trainSet['description'].apply(lambda x: [item for item in x if item not in stopwords])
 print("stopwords")
 print(trainSet.head())
 print()
+
+#remove stopwords in testSet 
 testSet['description'] = testSet['description'].apply(lambda x: [item for item in x if item not in stopwords])
 print(testSet.head())
 
-
+#use Lemmatizer for train and test set 
 lemmatizer = WordNetLemmatizer() 
 trainSet['description'] = trainSet['description'].apply(lambda x:[lemmatizer.lemmatize(word) for word in x])
 testSet['description'] = testSet['description'].apply(lambda x:[lemmatizer.lemmatize(word) for word in x])
@@ -94,26 +104,53 @@ print()
 trainSet1['description'] = trainSet1['description'].astype('str')
 testSet1['description'] = testSet1['description'].astype('str')
 
-#trainSet1.to_csv("./Civera/Data/action_train.csv", mode='w', index = False, header = False)
-#testSet1.to_csv("./Civera/Data/action_test.csv", mode='w', index = False, header = False)
+print("preprocessing done")
+print(trainSet1.head())
+print(testSet1.head())
 
 
+#train-test-split starts 
+X = trainSet1['description']
+y = trainSet1['action_index']
 
-print("done")
+print("train-test-split processing")
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size = 0.2, random_state = 42)
 
-# text_clf2 = Pipeline([('tfidf', TfidfVectorizer()),
-#                      ('rdf',RandomForestClassifier()),
-# ])
-# # Feed the training data through the pipeline
-# text_clf2.fit(X_train[:10000], y_train[:10000])
+clf1 = Pipeline([('tfidf', TfidfVectorizer()),('rdf',RandomForestClassifier()),])
+# training data through the pipeline
+clf1.fit(X_train, y_train)
+
+clf2 = Pipeline([('tfidf', TfidfVectorizer()),('mnb',MultinomialNB()),])
+# training data through the pipeline
+clf2.fit(X_train, y_train)
+
+#RandomForest Prediction 
+prediction1 = clf1.predict(testSet1['description'])
+print(prediction1.shape)
+print() 
+
+#MultinomialNB Prediction 
+prediction2 = clf2.predict(testSet1['description'])
+print(prediction2.shape)
+
+submission1 = pd.DataFrame({'actor':testSet1['actor'],'description':testSet1['description'],'action_index':prediction1})
+#Visualize the first 5 rows
+print("prediction 1")
+print(submission1.head())
+submission1 = submission1.merge(distinct_actions, on='action_index') 
+
+submission2 = pd.DataFrame({'actor':testSet1['actor'],'description':testSet1['description'],'action_index':prediction2})
+print("prediction 2")
+print(submission2.head())
+submission2 = submission2.merge(distinct_actions, on='action_index') 
+
+path4 = 'C:\\Users\\Serra\\Desktop\\CS506Spring2021Repository\\Civera\\Data\\RandomForest-Prediction.txt'
+path5 = 'C:\\Users\\Serra\\Desktop\\CS506Spring2021Repository\\Civera\\Data\\MultinomialNB-Prediction.txt'
+submission1.to_csv(path4, mode='w', index = False)
+submission2.to_csv(path5, mode='w', index = False)
+#testSet1.to_csv(path3, mode='w', index = False, header = False)
 
 
-#Preparing data
-X_train, X_test, Y_train, Y_test = train_test_split(
-        trainSet1['actor','description'],
-        trainSet1['action'],
-        test_size=1
-    )
 
 # # Process the DataFrames
 # # This is where you can do more feature extraction
