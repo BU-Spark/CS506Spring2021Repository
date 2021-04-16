3fe68bfdb2d5032485dcf53d0a133a9aa809ba3d
Eric South <ericjohnsouth@gmail.com>
2021-04-15 20:24:24 -0400

    removing folders we never used

--- 10_K_Risk_Evaluator/src/__init__.py
+++
@@ -1,9 +0,0 @@
-'''
---------------------------------------------------------------------------------
-<CS506 Project: 10-K Risk Evaluator>
-
-This folder contains source code python modules besides main.py
-
-Written by Evie Wan, Nicholas Mosca, Eric South
---------------------------------------------------------------------------------
-'''
